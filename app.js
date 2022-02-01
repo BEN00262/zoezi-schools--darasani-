f3e580a1f4fcc38f9583346b96df6b9855830fdc
john kerama <johnnesta2018@gmail.com>
2022-02-02 00:12:16 +0300

    disabled the school creation form

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended:true}))
 // simple routes
 app.use(express.static(path.join(__dirname, 'public/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
 });
 
