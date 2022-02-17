98c54749935b5a0533acefc49c6cba7503c95509
john kerama <johnnesta2018@gmail.com>
2022-02-17 17:05:59 +0300

    ui improvement

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended:true }))
 app.use(cors())
 
 // simple routes
+app.use('*/img', express.static(path.join(__dirname,'/public/img')));
 app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use("/api/school", SchoolRoute);
